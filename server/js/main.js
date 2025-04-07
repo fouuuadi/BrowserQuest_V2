c2d64ed35a58e6ed3e87bd7d3c150d74a80fe74a
fouuuadi <fouadlamnaouar18@gmail.com>
2025-04-07 17:05:21 +0200

    fix main.js

--- server/js/main.js
+++ server/js/main.js
@@ -1,103 +1,49 @@
-
 var fs = require('fs'),
     Metrics = require('./metrics');
 
-
-function main(config) {
-    var ws = require("./ws"),
-        WorldServer = require("./worldserver"),
-        Log = require('log'),
-        _ = require('underscore'),
-        server = new ws.MultiVersionWebsocketServer(config.port),
-        metrics = config.metrics_enabled ? new Metrics(config) : null;
-        worlds = [],
-        lastTotalPlayers = 0,
-        checkPopulationInterval = setInterval(function() {
-            if(metrics && metrics.isReady) {
-                metrics.getTotalPlayers(function(totalPlayers) {
-                    if(totalPlayers !== lastTotalPlayers) {
-                        lastTotalPlayers = totalPlayers;
-                        _.each(worlds, function(world) {
-                            world.updatePopulation(totalPlayers);
-                        });
-                    }
-                });
-            }
-        }, 1000);
+    function main(config) {
+        var log = console; // Utilisation de console pour la journalisation
     
-    switch(config.debug_level) {
-        case "error":
-            log = new Log(Log.ERROR); break;
-        case "debug":
-            log = new Log(Log.DEBUG); break;
-        case "info":
-            log = new Log(Log.INFO); break;
-    };
-    
-    log.info("Starting BrowserQuest game server...");
-    
-    server.onConnect(function(connection) {
-        var world, // the one in which the player will be spawned
-            connect = function() {
-                if(world) {
-                    world.connect_callback(new Player(connection, world));
+        var ws = require("./ws"),
+            WorldServer = require("./worldserver"),
+            _ = require('underscore'),
+            server = new ws.MultiVersionWebsocketServer(config.port),
+            metrics = config.metrics_enabled ? new Metrics(config) : null,
+            worlds = [],
+            lastTotalPlayers = 0,
+            checkPopulationInterval = setInterval(function() {
+                if (metrics && metrics.isReady) {
+                    metrics.getTotalPlayers(function(totalPlayers) {
+                        if (totalPlayers !== lastTotalPlayers) {
+                            lastTotalPlayers = totalPlayers;
+                            _.each(worlds, function(world) {
+                                world.updatePopulation(totalPlayers);
+                            });
+                        }
+                    });
                 }
-            };
-        
-        if(metrics) {
-            metrics.getOpenWorldCount(function(open_world_count) {
-                // choose the least populated world among open worlds
-                world = _.min(_.first(worlds, open_world_count), function(w) { return w.playerCount; });
-                connect();
-            });
-        }
-        else {
-            // simply fill each world sequentially until they are full
-            world = _.detect(worlds, function(world) {
-                return world.playerCount < config.nb_players_per_world;
-            });
-            world.updatePopulation();
-            connect();
-        }
-    });
-
-    server.onError(function() {
-        log.error(Array.prototype.join.call(arguments, ", "));
-    });
+            }, 1000);
     
-    var onPopulationChange = function() {
-        metrics.updatePlayerCounters(worlds, function(totalPlayers) {
-            _.each(worlds, function(world) {
-                world.updatePopulation(totalPlayers);
-            });
+        // Remplacez les niveaux de journalisation par console.log
+        switch (config.debug_level) {
+            case "error":
+                log.error = console.error; break;
+            case "debug":
+                log.debug = console.log; break;
+            case "info":
+                log.info = console.log; break;
+        }
+    
+        log.info("Starting BrowserQuest game server...");
+    
+        server.onError(function() {
+            log.error(Array.prototype.join.call(arguments, ", "));
         });
-        metrics.updateWorldDistribution(getWorldDistribution(worlds));
-    };
-
-    _.each(_.range(config.nb_worlds), function(i) {
-        var world = new WorldServer('world'+ (i+1), config.nb_players_per_world, server);
-        world.run(config.map_filepath);
-        worlds.push(world);
-        if(metrics) {
-            world.onPlayerAdded(onPopulationChange);
-            world.onPlayerRemoved(onPopulationChange);
-        }
-    });
     
-    server.onRequestStatus(function() {
-        return JSON.stringify(getWorldDistribution(worlds));
-    });
-    
-    if(config.metrics_enabled) {
-        metrics.ready(function() {
-            onPopulationChange(); // initialize all counters to 0 when the server starts
+        process.on('uncaughtException', function(e) {
+            log.error('uncaughtException: ' + e);
         });
     }
-    
-    process.on('uncaughtException', function (e) {
-        log.error('uncaughtException: ' + e);
-    });
-}
 
 function getWorldDistribution(worlds) {
     var distribution = [];
