8e38265b64c34f1bbfb8fe71c98a6ec0e043e6a4
Lm-hg <you@example.com>
2025-04-10 22:19:04 +0200

    mise en place du load balacing et deployment docker2

--- client/js/gameclient.js
+++ client/js/gameclient.js
@@ -45,7 +45,7 @@ define(['player', 'entityfactory', 'lib/bison'], function(Player, EntityFactory,
         },
         
         connect: function(dispatcherMode) {
-            var url = "ws://"+ this.host +":"+ this.port +"/api/",
+            var url = "ws://"+ this.host +":"+ this.port +"/api/", //fouad c'était ici le problème, après avoir configurer les port, on devait envoyer toutes requestes /api/
                 self = this;
             
             log.info("Trying to connect to server : "+url);
